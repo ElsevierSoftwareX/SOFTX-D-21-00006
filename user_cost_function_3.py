bba29e07939bd6900d5c3cdb66f2f4dc563e8eca
prince <prince.ms2110@gmail.com>
2020-07-11 20:02:16 +0200

    [ONLY FOR 2D] Use Scipy Voronoi function to create tessellations
    Scipy Voronoi function was used to create tessellations. Currently it
    can be used only for 2D case. The structural characteristics module was
    accordingly modified such that junction lengths and distance between
    neighbors could be computed for Scipy Voronoi function. The user defined
    custom cost function was also developed accordingly. This shows that the
    code is modular and modules can be replaced by the user as per his
    needs.

---
+++ user_cost_function_3.py
@@ -0,0 +1,45 @@
+import numpy as np
+import copy
+
+def mathematical_function(C, M, distArr, CritDist):
+    """
+    Input: Constants C & M, Array of lengths or distances, Critical length or 
+    distance.
+
+    Processing: Computes cost value based on exponential and linear functions. 
+    Basically penalizes seeds or vertices too close to each other and rewards 
+    seeds or vertices too far away from each other.
+
+    Returns: Sum of both exponential and linear terms
+    """
+    distCost=np.exp(-C * (distArr-CritDist)) + M * (distArr-CritDist)
+    #print('Critdist: ', CritDist, distCost); exit()
+    return np.sum(distCost)
+
+def function_formula(combined_user_data, combined_predicted_data, start_row_combined_data, data_dictionary, args):
+    """
+    Input: As specified in the documentation of Optimized Microstructure Generator
+    under section user-defined cost function.
+
+    Processing: Computes cost function by penalizing seeds & vertices that are 
+    very close and attracts them when they are very far away.
+
+    Returns: Sum of cost value obtained by considering both distance between grains
+    and junction length.
+    """
+    C = 0.6
+    M = 0.6
+    
+    #args_list = [parameter_list, dimension, user_data, start_row_of_parameter, limit, number_of_bins, fig_animate, ax_animate, cost_function_names, func_name_key, required_texture, rand_quat_flag, stress_direction, orientation_data, skewed_boundary_flag, tessellation]
+
+    CritDist_dist = 0.1*min(args[4][:args[1]])
+    CritDist_edge = 0.05*min(args[4][:args[1]])
+
+    ## Extracting data from the data_dictionary
+    all_distances = np.array(data_dictionary['5b'])
+    all_junction_lengths = np.array(data_dictionary['3'][:, 2])
+
+    cost_value_dist = mathematical_function(C, M, all_distances, CritDist_dist)
+    cost_value_edge = mathematical_function(C, M, all_junction_lengths, CritDist_edge)
+
+    return 0.5*(cost_value_dist + cost_value_edge)
